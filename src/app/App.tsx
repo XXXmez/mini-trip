0f9d009aa8dad366e6fe7aad38e7103e5ad7b93d
Ruslan Bogdanov <57019591+XXXmez@users.noreply.github.com>
2025-10-02 02:03:01 +0300

    feature [MT-1]: Убираем горизонтальные свайпы.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import {
   RouterProvider,
   ServiceWorkerProvider,
@@ -8,6 +9,24 @@ import './App.css';
 import './styles/themes.scss';
 
 function App() {
+  useEffect(() => {
+    const handler = (e: TouchEvent) => {
+      const touch = e.touches[0];
+      const edgeZone = 30; // px от края экрана
+
+      if (
+        touch.clientX < edgeZone ||
+        touch.clientX > window.innerWidth - edgeZone
+      ) {
+        // свайп начинается у края → блокируем
+        e.preventDefault();
+      }
+    };
+
+    document.addEventListener('touchstart', handler, { passive: false });
+    return () => document.removeEventListener('touchstart', handler);
+  }, []);
+
   return (
     <div className={'app'}>
       <ServiceWorkerProvider>
